9a38207c875669f9361876e160c4fb45048167b5
DatSpookTho <nickguin123@gmail.com>
2017-05-28 19:34:05 -0400

    Pwogg.

--- commands.js
+++ commands.js
@@ -29,57 +29,58 @@ let commands = {
 	},
 	
 	beep: function (target, room, user) {
-		if (room !== user && !user.hasRank(room, '+')) return;
+		if (!user.hasRank(room, '+')) return;
 		this.say("boop");
 	},
 
 	argbeep: function (arg, target, room, user) {
 		if (room !== user && !user.hasRank(room, '+')) return;
-		Client.send("/me boops" + arg);
+		Client.send("scrabble|/me boops " + arg);
 	},
 	
 	intro: function (target, room, user) {
-		if (room !== user && !user.hasRank(room, '+')) return;
-		Client.send("scrabble|!htmlbox <div>Welcome to Scrabble! <li><a href='https://docs.google.com/document/d/1tUmTwieP-cdfaThcVVULmpa-4G_yVu6sXs_W7947o1U/edit'>Room Rules</a></li> <li> <a href='https://docs.google.com/document/d/1NrWGokTArxCd0GXeKMf3oWEZb7wDcRA7U9g_6uWPm9s/edit'>Scrabblemon Rules</a></li> <li><a href='https://pastebin.com/Grcz7NUQ'>Userhosted Games</a></li></ul></div>");
+		if (!user.hasRank(room, '+')) return;
+		Client.send("scrabble|!addhtmlbox <div>Welcome to Scrabble!<li> <a href='http://scrabble-ps.weebly.com'>PS Scrabble Website</a></li> <li><a href='https://en.crosswordsarena.com/'>Play Scrabble!</a></li></ul></div>");
 	},
-	
-	lingo: function (arg, target, room, user) {
-		if (room !== user && !user.hasRank(room, '+')) return;
-		this.say("/wall " + arg + " is hosting a Scrabgame of Lingo! Use ``/me in`` to join!");
-		this.say("The host selects a 5 letter word and reveals the first letter, players then guess possible answers. The word is Codes, so it would be C _ _ _ _. If Couch is guessed, it would then be C O _ _ _. Game Guide: https://docs.google.com/document/d/19VF4pRtkIMx9PEySWQShXr_NwCWZp17h1K_eUlcSBcU/edit?usp=sharing")
-	},
-	
-	ww: function (arg, target, room, user) {
-		if (room !== user && !user.hasRank(room, '+')) return;
-		this.say("/wall " + arg + " is hosting a Scrabgame of Watchog's Words! Use ``/me in`` to join!");
-		this.say("Players receive 9 letters and must PM the host a word that can be formed using these letters. Points are awarded based on the length of the word. Game Guide: https://docs.google.com/document/d/1Rt2a-H1eXiMo2eqRMbLbtp-xHx_93XWGARXeX8INcnI/edit?usp=sharing")
-	},
-	
-	wl: function (arg, target, room, user) {
-		if (room !== user && !user.hasRank(room, '+')) return;
-		this.say("/wall " + arg + " is hosting a Scrabgame of Word Ladder! Use ``/me in`` to join!");
-		this.say("Players are presented with a 4 letter word, and they may only change one letter to make a new word! CAPE - CAVE - COVE - CONE - CORE. Game Guide: https://docs.google.com/document/d/1iljWB7LpaIbW3p2c_tm2HlYnzlBp01mcgdQNFEAcz_E/edit?usp=sharing")
-	},
-	
-	win: function (arg, target, room, user) {
-		if (room !== user && !user.hasRank(room, '+')) return;
-		this.say("The winner is " + arg + "! Thanks for playing!")
-		Client.send("scrabble|/mn Gamewinner: " + arg)
-	},
-	
-	end: function (target, room, user) {
-		if (room !== user && !user.hasRank(room, '+')) return;
-		this.say("The game was ended.")
-	},
+
 	roomdesc: function (target, room, user) {
-		if (room !== user && !user.hasRank(room, '+')) return;
-		Client.send("scrabble|!htmlbox <div>Play Scrabble with friends, participate in or host fun Word Games, and compete in the exciting new Scrabblemons metagame!</div>");
+		if (!user.hasRank(room, '+')) return;
+		Client.send("scrabble|!addhtmlbox <div>Play Scrabble with friends, join Scrabble tournaments, and compete in the exciting new Scrabblemons metagame!</div>");
 	},
 	
 	nt: function (target, room, user) {
-		if (room !== user && !user.hasRank(room, '+')) return;
+		if (!user.hasRank(room, '+')) return;
 		this.say("this is why I don't play with timer.");
 	},
+	
+	say: function (arg, target, room, user) {
+		if (room !== user || !user.hasRank(room, '+')) return;
+		this.say(arg);
+	},
+	
+	commands: function (target, room, user) {
+		if (room !== user && !user.hasRank(room, '+')) return;
+		this.say("https://hastebin.com/zebacaxesu.vbs");
+	},
+	
+	wotd: function (target, room, user) {
+		if (room !== user && !user.hasRank(room, '+')) return;
+		Client.send('scrabble|!addhtmlbox <img src="http://i.imgur.com/RyoZ1GA.png" height="89" width="160"><div>ˈ<b> Word of the Day:</b> /äˌä/ (Noun) basaltic lava forming very rough jagged masses with a light frothy texture. <b>Scrabble Points:</b> 2 </div>');
+	},
+	
+	tour: function (target, room, user) {
+		if (room !== user && !user.hasRank(room, '%')) return;
+		Client.send('scrabble|/tour create gen 7 ubers, elimination');
+		Client.send('scrabble|/wall This is a Scrabblemons Tour! Only use a Scrabblemons team! Info on Scrabblemons can be found here: http://scrabble-ps.weebly.com/scrabble-mons-guide.html. Using banned Pokemon are strictly prohibited, and you will be disqualified if you are caught using one.');
+
+	},
+	
+	tourstart: function (target, room, user) {
+		if (room !== user && !user.hasRank(room, '%')) return;
+		Client.send('scrabble|/tour start');
+		Client.send('scrabble|/wall Good luck to everyone!');
+	},
+	
 	
 	//client.send("scrabble|/mn	
 	// Game commands
@@ -111,6 +112,42 @@ let commands = {
 		room.game.leave(user);
 	},
 
+	//memes
+	topaz: function (target, room, user) {
+		if (!user.hasRank(room, '+')) return;
+		Client.send("scrabble|!dt Excadrill");
+	},
+	
+	arandom: function (target, room, user) {
+		if (!user.hasRank(room, '+')) return;
+		Client.send("scrabble|!dt Ducklett");
+	},
+	
+	spook: function (target, room, user) {
+		if (!user.hasRank(room, '+')) return;
+		Client.send("scrabble|!dt Heatmor");
+	},
+	
+	snap: function (target, room, user) {
+		if (!user.hasRank(room, '+')) return;
+		Client.send("scrabble|!dt Misdreavus");
+	},
+	
+	dsg: function (target, room, user) {
+		if (!user.hasRank(room, '+')) return;
+		Client.send("scrabble|!dt Giratina-Origin");
+	},
+
+	order: function (target, room, user) {
+		if (!user.hasRank(room, '+')) return;
+		Client.send("scrabble|!dt Zygarde-10%");
+	},
+	
+	azu: function (target, room, user) {
+		if (!user.hasRank(room, '+')) return;
+		Client.send("scrabble|!dt Celebi");
+	},
+	
 	// Storage commands
 	bits: 'points',
 	points: function (target, room, user) {
