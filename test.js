722b54aec5d333a6034256709bbeacdd86a44c94
Luke Vellotti <meesechuffin@gmail.com>
2018-01-30 07:55:36 -0800

    Remove test file

--- test.js
+++
@@ -1,7 +0,0 @@
-const words = require('./wordslmao.js').scrabwords;
-const pokemon = require('./data/pokedex.js').BattlePokedex;
-
-for (let i in pokemon) {
-    const realword = i.toUpperCase();
-    if (words.indexOf(realword) !== -1) console.log(i);
-}
